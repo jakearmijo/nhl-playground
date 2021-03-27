0b416d7a8775ec28f7805521fcd9729a4d5ce94f
Jake Armijo <armijojake@yahoo.com>
2021-03-26 21:53:54 -0500

    chore: stats comp rough start

--- src/UI/molecules/StatsTable/StatsTable.js
+++ src/UI/molecules/StatsTable/StatsTable.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { StatsGrid, StatsGridItem } from './StatsTable.style'
 
 export default function StatsTable() {
     return (
-        <div>
-            
-        </div>
+      <>
+
+      <StatsGrid class="grid-container">
+        <StatsGridItem class="grid-item">1</StatsGridItem>
+        <StatsGridItem class="grid-item">2</StatsGridItem>
+        <StatsGridItem class="grid-item">3</StatsGridItem>  
+        <StatsGridItem class="grid-item">4</StatsGridItem>
+        <StatsGridItem class="grid-item">5</StatsGridItem>
+        <StatsGridItem class="grid-item">6</StatsGridItem>  
+        <StatsGridItem class="grid-item">7</StatsGridItem>
+        <StatsGridItem class="grid-item">8</StatsGridItem>
+        <StatsGridItem class="grid-item">9</StatsGridItem>  
+      </StatsGrid>
+      </>
     )
 }
--- src/UI/molecules/StatsTable/StatsTable.style.js
+++ src/UI/molecules/StatsTable/StatsTable.style.js
@@ -1,53 +1,17 @@
 import styled from 'styled-components'
-import {
-  zdSpacingXs,
-  zdSpacingXxs,
-} from '@zendeskgarden/css-variables'
-import { Table, Row } from '@zendeskgarden/react-tables'
 
 
-export const StandingsItemWrapper = styled(Table)`
-  border-radius: ${zdSpacingXxs};
-  &:nth-of-type(odd) {
-    padding-right: 2px;
-  }
-  &:nth-of-type(even) {
-    padding-left: 2px;
-  }
-  ${({ isInherited }) => (
-    isInherited && `opacity: 0.4;`
-  )}
-`
-export const StandingsTile = styled(Row)`
-  min-height: 60px;
-  padding: ${zdSpacingXs};
-  margin-bottom: ${zdSpacingXxs};
-  display: flex;
-  flex-direction: column;
-  background-color: white;
-`
-export const StyledH2 = styled.h2`
-font-size: 36px;
-font-weight: bold;
-justify-content: left;
-float: left;
+
+
+export const StatsGrid = styled.div`
+  display: grid;
+  grid-template-columns: auto auto auto;
+  padding: 2px;
 `
 
-export const StyledH4 = styled.h4`
-  display: block;
-  font-size: 1.17em;
-  margin-top: 1em;
-  margin-bottom: 1em;
-  margin-left: 0;
-  margin-right: 0;
-  font-weight: bold;
-`
-
-export const StyledDiv = styled.div`
-  display: flex;
-  flex-direction: row;
-`
-
-export const StyledDivContainer = styled.div`
-  display: flex;
+export const StatsGridItem = styled.div`
+  border: 1px solid rgba(0, 0, 0, 0.8);
+  padding: 2px;
+  font-size: 7px;
+  text-align: center;
 `

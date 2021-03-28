3501b873c2d209c5bb8d6b0a9ece2734d2784e7e
Jake Armijo <armijojake@yahoo.com>
2021-03-27 21:15:51 -0500

    feat: rough cut for stats comp

--- src/UI/molecules/StatsTable/StatsTable.js
+++ src/UI/molecules/StatsTable/StatsTable.js
@@ -3,19 +3,30 @@ import { StatsGrid, StatsGridItem } from './StatsTable.style'
 
 export default function StatsTable() {
     return (
-      <>
+      <div>
+        <StatsGrid class="grid-container">
+          <StatsGridItem class="grid-item">Team 1</StatsGridItem>
+          <StatsGridItem class="grid-item">Shots on Goal</StatsGridItem>  
+          <StatsGridItem class="grid-item">Faceoff %</StatsGridItem>
+          <StatsGridItem class="grid-item">Power Play</StatsGridItem>
+          <StatsGridItem class="grid-item">PIM</StatsGridItem>  
+          <StatsGridItem class="grid-item">Hits</StatsGridItem>
+          <StatsGridItem class="grid-item">Blocks</StatsGridItem>
+          <StatsGridItem class="grid-item">Giveaways</StatsGridItem>  
+          <StatsGridItem class="grid-item">Takeaways</StatsGridItem>
+        </StatsGrid>
 
-      <StatsGrid class="grid-container">
-        <StatsGridItem class="grid-item">1</StatsGridItem>
-        <StatsGridItem class="grid-item">2</StatsGridItem>
-        <StatsGridItem class="grid-item">3</StatsGridItem>  
-        <StatsGridItem class="grid-item">4</StatsGridItem>
-        <StatsGridItem class="grid-item">5</StatsGridItem>
-        <StatsGridItem class="grid-item">6</StatsGridItem>  
-        <StatsGridItem class="grid-item">7</StatsGridItem>
-        <StatsGridItem class="grid-item">8</StatsGridItem>
-        <StatsGridItem class="grid-item">9</StatsGridItem>  
-      </StatsGrid>
-      </>
+        <StatsGrid class="grid-container">
+          <StatsGridItem class="grid-item">Team 2</StatsGridItem>
+          <StatsGridItem class="grid-item">Shots on Goal</StatsGridItem>  
+          <StatsGridItem class="grid-item">Faceoff %</StatsGridItem>
+          <StatsGridItem class="grid-item">Power Play</StatsGridItem>
+          <StatsGridItem class="grid-item">PIM</StatsGridItem>  
+          <StatsGridItem class="grid-item">Hits</StatsGridItem>
+          <StatsGridItem class="grid-item">Blocks</StatsGridItem>
+          <StatsGridItem class="grid-item">Giveaways</StatsGridItem>  
+          <StatsGridItem class="grid-item">Takeaways</StatsGridItem>
+        </StatsGrid>
+      </div>
     )
 }
--- src/UI/molecules/StatsTable/StatsTable.style.js
+++ src/UI/molecules/StatsTable/StatsTable.style.js
@@ -5,13 +5,13 @@ import styled from 'styled-components'
 
 export const StatsGrid = styled.div`
   display: grid;
-  grid-template-columns: auto auto auto;
+  grid-template-columns: auto auto auto auto auto auto auto auto auto;
   padding: 2px;
 `
 
 export const StatsGridItem = styled.div`
   border: 1px solid rgba(0, 0, 0, 0.8);
   padding: 2px;
-  font-size: 7px;
+  font-size: 15px;
   text-align: center;
 `

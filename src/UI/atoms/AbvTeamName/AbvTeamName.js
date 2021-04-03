b31225dc676b68c633f4a0ad0983efc1a37e1c67
Jake Armijo <armijojake@yahoo.com>
2021-04-02 23:12:49 -0500

    feat: rendering abv for stats

--- src/UI/atoms/AbvTeamName/AbvTeamName.js
+++ src/UI/atoms/AbvTeamName/AbvTeamName.js
@@ -6,37 +6,20 @@ import { ensureObject } from '../../../lib'
 // let color = 'grey' | 'red' | 'green' | 'yellow'
 
 const AbvTeamName = ( { teamId } ) => {
-console.log("🚀 ~ file: AbvTeamName.js ~ line 9 ~ AbvTeamName ~ teamId", teamId)
   const [abv, setTeamAbv] = useState()
+
   useEffect(() => {
-    const fetchData = async () => {
-      await fetch(
-        `https://statsapi.web.nhl.com/api/v1/teams/${teamId}`
-      )
-        .then((res) => res.json())
-        .then(
-          (result) => {
-          
-            setTeamAbv({
-              abv: result,
-            });
-              console.log("🚀 ~ file: AbvTeamName.js ~ line 23 ~ result", result.teams[0].abbreviation)
-              console.log("🚀 ~ file: AbvTeamName.js ~ line 23 ~ abv", abv)
-              
-          },
-          (error) => {
-            setTeamAbv({
-              error,
-            });
-          }
-        );
-    };
-    fetchData();
-  }, [abv, teamId]);
+    fetch(`https://statsapi.web.nhl.com/api/v1/teams/${teamId}`)
+    .then( res => res.json())
+    .then( result => {
+      setTeamAbv({ abv: result.teams[0].abbreviation })
+    })
+  }, []);
+
   return (
   <div>
     <h6>
-    {teamId} 
+    {ensureObject(abv).abv}
     </h6>
   </div>
   )
